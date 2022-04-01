c1ca40476f8ba3af44cef473e41e56c52200e6d4
Zanarelli <gzanarellipro@gmail.com>
2022-04-01 15:21:50 +0200

    download

--- src/App.js
+++ src/App.js
@@ -1,14 +1,49 @@
+import { Button } from '@mui/material';
+import _ from 'lodash';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import './App.css';
 import { TextInput } from './Input';
 import { SelectInput } from './Select';
 
+const verifiedNumberValue = (value) => {
+  if (value && value !== undefined) {
+    const matchLetter = value?.match(/[a-zA-Z]/);
+    const matchSpecialCaracters = value?.match(/[^.\w]/);
+    const matchDotWithoutNumber = value?.match(/(^\.|\.$)/);
+
+    const matches = _.union(matchLetter, matchSpecialCaracters, matchDotWithoutNumber);
+    if (matches && matches.length > 0) {
+      return false;
+    }
+    return true;
+  }
+  return false;
+};
+
+
 function App() {
 
-  const {handleSubmit, register, control, watch} = useForm()
-  const onSubmit = (values) => console.log(values)
+  const { handleSubmit, control, watch, setValue } = useForm()
 
+  const [fileDownloadUrl, setFileDownloadUrl] = useState(null)
   const typeProductionEcs = watch('natureOfEcsProduction')
+  const typeEnergie = watch('energyType')
+
+  const onSubmit = (values) => {
+    let tmp = {}
+    _.forOwn(values, (value, key) => {
+      if (verifiedNumberValue(value)) {
+        tmp = { ...tmp, [key]: parseFloat(value) }
+      } else {
+        tmp = { ...tmp, [key]: value }
+      }
+    })
+    console.log(tmp)
+    const blob = new Blob([JSON.stringify(tmp)]);
+    setFileDownloadUrl(URL.createObjectURL(blob));
+    this.dofileDownload.click()
+  }
 
   return (
     <div className="App"
@@ -30,17 +65,21 @@ function App() {
             label='Nature de la production ECS'
             control={control}
             options={[
-              {value: "shared", label: "Partagée"},
-              {value: "individual", label: "Individuelle"},
-              {value: "separate", label: "Séparée"},
+              { value: "shared", label: "Partagée" },
+              { value: "individual", label: "Individuelle" },
+              { value: "separate", label: "Séparée" },
             ]}
           />
           <TextInput
+            setValue={setValue}
+            type='number'
             name='year'
             label='Année'
             control={control}
           />
           <TextInput
+            setValue={setValue}
+            type='number'
             name='generalCharges'
             label='Charges générales'
             control={control}
@@ -50,31 +89,38 @@ function App() {
             label='Energie'
             control={control}
             options={[
-              {value: "naturalGas", label: "Gaz naturel"},
-              {value: "oil", label: "Fioul"},
-              {value: "rcu", label: "RCU"},
-              {value: "propane", label: "Propane"},
-              {value: "biopropane", label: "Bio propane"},
-              {value: "greenGaz", label: "Gaz vert"},
-              {value: "butane", label: "Butane"},
-              {value: "granulatedWood", label: "Granulé de bois"},
-              {value: "woodChips", label: "Bois"},
-              {value: "electricity", label: "Electricité"},
-              {value: "other", label: "Autre"},
+              { value: "naturalGas", label: "Gaz naturel" },
+              { value: "oil", label: "Fioul" },
+              { value: "rcu", label: "RCU" },
+              { value: "propane", label: "Propane" },
+              { value: "biopropane", label: "Bio propane" },
+              { value: "greenGaz", label: "Gaz vert" },
+              { value: "butane", label: "Butane" },
+              { value: "granulatedWood", label: "Granulé de bois" },
+              { value: "granulatedWood", label: "Bois" },
+              { value: "electricity", label: "Electricité" },
+              { value: "other", label: "Autre" },
             ]}
           />
           <TextInput
+            setValue={setValue}
+            type='number'
             name='combustibleConsumption'
-            label='consommation'
+            label={`consommation ${['granulatedWood', 'granulatedWood'].includes(typeEnergie) ?
+              '(T)' : ['oil'].includes(typeEnergie) ? '(L)' : '(Kwh)'}`}
             control={control}
           />
 
           <TextInput
+            setValue={setValue}
+            type='number'
             name='combustibleCost'
             label='coût p1 (€)'
             control={control}
           />
           <TextInput
+            setValue={setValue}
+            type='number'
             name='ecsCost'
             label='coût ECS (€)'
             control={control}
@@ -83,6 +129,8 @@ function App() {
             typeProductionEcs === "separate" ? (
               <>
                 <TextInput
+                  setValue={setValue}
+                  type='number'
                   name='combustibleConsumptionEcs'
                   label='consommation ECS (kWh)'
                   control={control}
@@ -92,39 +140,60 @@ function App() {
                   label="Type d'énergie ECS"
                   control={control}
                   options={[
-                    {value: "naturalGas", label: "Gaz naturel"},
-                    {value: "oil", label: "Fioul"},
-                    {value: "rcu", label: "RCU"},
-                    {value: "propane", label: "Propane"},
-                    {value: "biopropane", label: "Bio propane"},
-                    {value: "greenGaz", label: "Gaz vert"},
-                    {value: "butane", label: "Butane"},
-                    {value: "granulatedWood", label: "Granulé de bois"},
-                    {value: "woodChips", label: "Bois"},
-                    {value: "electricity", label: "Electricité"},
-                    {value: "other", label: "Autre"},
+                    { value: "naturalGas", label: "Gaz naturel" },
+                    { value: "oil", label: "Fioul" },
+                    { value: "rcu", label: "RCU" },
+                    { value: "propane", label: "Propane" },
+                    { value: "biopropane", label: "Bio propane" },
+                    { value: "greenGaz", label: "Gaz vert" },
+                    { value: "butane", label: "Butane" },
+                    { value: "granulatedWood", label: "Granulé de bois" },
+                    { value: "woodChips", label: "Bois" },
+                    { value: "electricity", label: "Electricité" },
+                    { value: "other", label: "Autre" },
                   ]}
                 />
               </>
             ) : null
           }
           <TextInput
+            setValue={setValue}
+            type='number'
             name='ecsVolume'
             label='volume ECS (m3)'
             control={control}
           />
           <TextInput
+            setValue={setValue}
+            type='number'
             name='dju'
             label='DJU'
             control={control}
           />
           <TextInput
+            setValue={setValue}
+            type='number'
             name='investmentMade'
             label='Investissements effectués (€)'
             control={control}
           />
         </section>
-        <button type="submit">Valider</button>
+        {
+          fileDownloadUrl ? (
+            <Button
+              download={'rafe-suivi.json'}
+              href={fileDownloadUrl}
+            >
+              Telecharger
+            </Button>
+
+          ) : null
+        }
+        <Button
+          type="submit"
+        >
+          Valider
+        </Button>
       </form>
     </div>
   );
--- src/Input.js
+++ src/Input.js
@@ -1,12 +1,16 @@
 import { TextField } from '@mui/material';
 import { Controller } from 'react-hook-form';
-export const TextInput = ({name, control, label, type = 'string'}) => {
+export const TextInput = ({ name, control, label, type = 'string' }) => {
+
   return (
-    <Controller 
+    <Controller
       name={name}
       control={control}
-      render={({field}) => {
-        return <TextField {...field} label={label} style={{marginTop: 10 }}/>
+      render={({ field }) => {
+        return <TextField
+          {...field}
+          label={label}
+          style={{ marginTop: 10 }} />
       }}
     />
   )
